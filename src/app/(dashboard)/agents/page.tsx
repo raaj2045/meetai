9432444cbab10214e78b1e27fb411d7db60b54bd
Raaj Anand Mishra <rmishra76@gatech.edu>
2025-05-30 15:36:33 +0530

    add agents form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,28 +1,43 @@
 import { Suspense } from "react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { ErrorBoundary } from "react-error-boundary";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
+import { auth } from "@/lib/auth";
 import { AgentsView } from "@/modules/agents/ui/views/agents-view"
 import { getQueryClient, trpc } from "@/trpc/server";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 
 const Page = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+
+    if (!session) {
+        redirect("/sign-in");
+    }
+
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
 
 
     return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<LoadingState title="Loading Agents" description="This may take some time..." />}>
-                <ErrorBoundary fallback={<ErrorState
-                    title="Error Loading Agents"
-                    description="Please try again later"
-                />}>
-                    <AgentsView />
-                </ErrorBoundary>
-            </Suspense>
-        </HydrationBoundary>
+        <>
+            <AgentsListHeader />
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Suspense fallback={<LoadingState title="Loading Agents" description="This may take some time..." />}>
+                    <ErrorBoundary fallback={<ErrorState
+                        title="Error Loading Agents"
+                        description="Please try again later"
+                    />}>
+                        <AgentsView />
+                    </ErrorBoundary>
+                </Suspense>
+            </HydrationBoundary>
+        </>
     );
 }
 
