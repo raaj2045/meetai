74eca3015e56d7dfe3b409663cd90f592eee657f
Raaj Anand Mishra <rmishra76@gatech.edu>
2025-06-01 21:07:03 +0530

    add edit and delete agents

--- src/modules/agents/ui/components/agent-form.tsx
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -38,6 +38,21 @@ export const AgentForm = ({ onSuccess, onCancel, initialValues }: AgentFormProps
                     trpc.agents.getMany.queryOptions({})
                 );
 
+                onSuccess?.();
+            },
+            onError: (error) => {
+                toast.error(error.message);
+            }
+        })
+    )
+
+    const updateAgent = useMutation(
+        trpc.agents.update.mutationOptions({
+            onSuccess: async () => {
+                await queryClient.invalidateQueries(
+                    trpc.agents.getMany.queryOptions({})
+                );
+
                 if (initialValues?.id) {
                     await queryClient.invalidateQueries(
                         trpc.agents.getOne.queryOptions({
@@ -63,11 +78,11 @@ export const AgentForm = ({ onSuccess, onCancel, initialValues }: AgentFormProps
     });
 
     const isEdit = !!initialValues?.id;
-    const isPending = createAgent.isPending;
+    const isPending = createAgent.isPending || updateAgent.isPending;
 
     const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
         if (isEdit) {
-
+            updateAgent.mutate({ ...values, id: initialValues.id })
         } else {
             createAgent.mutate(values);
         }
