4a9fc909c317a4a118d82874016d1aa0b33447e3
Raaj Anand Mishra <rmishra76@gatech.edu>
2025-06-02 22:00:32 +0530

    add meeting variants

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -8,6 +8,10 @@ import { MeetingIdViewHeader } from "../components/meeting-id-view-header";
 import { useConfirm } from "@/hooks/use-confirm";
 import { toast } from "sonner";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 
 interface Props {
@@ -23,7 +27,7 @@ export const MeetingIdView = ({ meetingId }: Props) => {
         trpc.meetings.getOne.queryOptions({ id: meetingId })
     )
 
- const [updateMeetingDialogOpen, setUpdateMeetingDialogOpen] = useState(false);
+    const [updateMeetingDialogOpen, setUpdateMeetingDialogOpen] = useState(false);
 
     const [RemoveConfirmation, confirmRemove] = useConfirm(
         "Are you sure?",
@@ -42,18 +46,24 @@ export const MeetingIdView = ({ meetingId }: Props) => {
         })
     )
 
-    const handleRemove = async () => {
+    const handleRemoveMeeting = async () => {
         const ok = await confirmRemove();
 
-        if(!ok) return;
+        if (!ok) return;
 
         await removeMeeting.mutateAsync({ id: meetingId });
     }
 
+    const isActive = data.status === "active";
+    const isUpcoming = data.status === "upcoming";
+    const isCancelled = data.status === "cancelled";
+    const isCompleted = data.status === "completed";
+    const isProcessing = data.status === "processing";
+
     return (
         <>
             <RemoveConfirmation />
-            <UpdateMeetingDialog 
+            <UpdateMeetingDialog
                 open={updateMeetingDialogOpen}
                 onOpenChange={setUpdateMeetingDialogOpen}
                 initialValue={data}
@@ -63,9 +73,19 @@ export const MeetingIdView = ({ meetingId }: Props) => {
                     meetingId={meetingId}
                     meetingName={data.name}
                     onEdit={() => setUpdateMeetingDialogOpen(true)}
-                    onRemove={handleRemove}
+                    onRemove={handleRemoveMeeting}
                 />
-                {JSON.stringify(data, null ,2)}
+                {isCancelled && <CancelledState />}
+                {isProcessing && <ProcessingState />}
+                {isActive && <ActiveState meetingId={meetingId} />}
+                {isUpcoming &&
+                    <UpcomingState
+                        meetingId={meetingId}
+                        onCancelMeeting={() => { }}
+                        isCancelling={false}
+                    />}
+                {isCompleted && <div>Completed</div>}
+
             </div>
         </>
 
